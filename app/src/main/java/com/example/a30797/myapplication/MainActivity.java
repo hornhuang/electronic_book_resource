9b57c92d1529f96eddeaeb6f102f13fbb4b6f4ff
LonglyWolf <814484626@qq.com>
2018-12-09 23:27:04 +0800

    YuanHao's second text ： 1.改变按钮为ImageButton 2.监听事件收起

--- app/src/main/java/com/example/a30797/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a30797/myapplication/MainActivity.java
@@ -4,16 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private  ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.text_World_Forst);
+        button = (ImageButton) findViewById(R.id.text_World_Forst);
+
+        Toast.makeText(MainActivity.this,"CSTI 神风测试已经开始！",Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void iniOnClick(){
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.bu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast
-                        .makeText(MainActivity.this,"!23",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"!23",Toast.LENGTH_SHORT).show();
             }
         });
     }
