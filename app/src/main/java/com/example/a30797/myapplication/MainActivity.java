7c3e912fe306fe61c86941ea42cad586c34b4dda
LonglyWolf <814484626@qq.com>
2018-12-09 23:03:00 +0800

    you

--- app/src/main/java/com/example/a30797/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a30797/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.a30797.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.text_World_Forst);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"超级牛逼大老宋",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
